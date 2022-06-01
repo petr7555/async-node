eaf79d23f19c89c7b632fbe66a7825791ecade94
Petr Janik <peta.janik@email.cz>
2022-06-01 12:01:55 +0200

    Fix check for error code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const runService = (workerData) => new Promise((resolve, reject) => {
     const worker = new Worker('./sample.workerthread.js', {workerData});
     worker.on('error', reject);
     worker.on('exit', (code) => {
-        if (code === 0) {
+        if (code !== 0) {
             reject(new Error(`error code ${code}`))
         }
     });
